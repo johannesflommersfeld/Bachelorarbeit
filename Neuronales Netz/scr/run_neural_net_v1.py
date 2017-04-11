fba227c0a39d919b4303cd4a1ddfb0dc0900bc20
Johannes Flommersfeld <johannes.flommersfeld@gmx.de>
2017-04-11 09:39:51 +0200

    Die Hilfsprogramme zum starten des Trainingsprozesses wurden aus dem Git-Verzeichnis entfernt

--- Neuronales Netz/scr/run_neural_net_v1.py
+++
@@ -1,5 +0,0 @@
-import mnist_loader
-training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-import network_v1
-net = network_v1.Network([784, 30, 10])
-net.SGD(training_data, 50, 10, 0.3, 1.0, lmbda = 5.0,test_data=test_data)
